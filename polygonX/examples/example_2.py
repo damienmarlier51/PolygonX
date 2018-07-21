e18c5c72b889686dfb6b0425615a826afd09bbfb
damienm-tessa <damienmarlier@tessatherapeutics.com>
2018-07-21 16:12:12 +0800

    Refactoring library

---
+++ polygonX/examples/example_2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import polygonX as pgx
+import matplotlib.pylab as plt
+
+def run_example_2():
+
+	df = pd.read_csv("metropolitan_french_cities.csv",header=None)
+	df = df.sample(n=3000)
+	points = df[[1,2]].values
+
+	for l in [0.5,1,3]:
+		plt.scatter([x[0] for x in points],[x[1] for x in points],s=1)
+		edges = pgx.Polygon.draw(points,l)
+		for edge in edges:
+			plt.plot([points[edge[0]][0], points[edge[1]][0]], [points[edge[0]][1], points[edge[1]][1]], color='red')
+		plt.title('l = %.2f' % l)
+		plt.show()
+
+if __name__ == "__main__":
+
+	run_example_2()
